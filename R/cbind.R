49c659767cb385b7ed1ed157c477c6a180fbef99
Herve Pages <hpages@fhcrc.org>
2011-11-10 07:36:04 +0000

    Add cbind() and rbind() generics.
    git-svn-id: file:///home/git/hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/BiocGenerics@60176 bc3139a8-67e5-0310-9ffc-ced21a209358

---
+++ R/cbind.R
@@ -0,0 +1,10 @@
+###
+
+setGeneric("cbind", signature="...",
+    function(..., deparse.level=1) standardGeneric("cbind")
+)
+
+setGeneric("rbind", signature="...",
+    function(..., deparse.level=1) standardGeneric("rbind")
+)
+
