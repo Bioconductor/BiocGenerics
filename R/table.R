0b2e12fd2de7157b013850ee301a47ae7cce6c44
Herve Pages <hpages@fhcrc.org>
2011-11-10 23:52:15 +0000

    Remove the extra args of base::table() from the args of table() generic.
    git-svn-id: file:///home/git/hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/BiocGenerics@60217 bc3139a8-67e5-0310-9ffc-ced21a209358

--- R/table.R
+++ R/table.R
@@ -2,11 +2,23 @@
 ### The table() generic
 ### -------------------------------------------------------------------------
 
+### base::table() has a broken signature (list.names() is a function
+### defined *inside* the body of base::table() so the default value for the
+### 'dnn' arg is an expression that cannot be evaluated *outside* the
+### base::table environment, this is poor design), we cannot keep all the
+### extra arguments in the table() generic (those extra arguments are ugly
+### and nobody uses them anyway).
+#setGeneric("table", signature="...",
+#    function(..., exclude = if (useNA == "no") c(NA, NaN),
+#                  useNA = c("no", "ifany", "always"),
+#                  dnn = list.names(...),
+#                  deparse.level = 1)
+#        standardGeneric("table")
+#)
+
+### So we use this instead.
 setGeneric("table", signature="...",
-    function(..., exclude = if (useNA == "no") c(NA, NaN),
-                  useNA = c("no", "ifany", "always"),
-                  dnn = list.names(...),
-                  deparse.level = 1)
-        standardGeneric("table")
+    function(...) standardGeneric("table"),
+    useAsDefault = function(...) base::table(...)
 )
 
