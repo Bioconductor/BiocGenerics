8c3cd9958ebc0b8ee64520f9c45ca329c2d53f42
Herve Pages <hpages@fhcrc.org>
2011-11-16 21:14:53 +0000

    oops, typo
    git-svn-id: file:///home/git/hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/BiocGenerics@60401 bc3139a8-67e5-0310-9ffc-ced21a209358

--- R/eval.R
+++ R/eval.R
@@ -6,7 +6,7 @@
 ### package "base" would dispatch on all the arguments. Here we set dispatch
 ### on the first two args (the 'expr' and 'envir' args) only!
 
-setGeneric("eval", signature("expr", "envir"),
+setGeneric("eval", signature=c("expr", "envir"),
     function(expr, envir=parent.frame(),
                    enclos=if (is.list(envir) || is.pairlist(envir))
                           parent.frame() else baseenv())
