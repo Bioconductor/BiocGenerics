c9c8282644e87fb3fe88c9b94cf5696fa8fa2c5a
Herve Pages <hpages@fhcrc.org>
2011-11-17 07:29:47 +0000

    Add the unique() generic.
    git-svn-id: file:///home/git/hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/BiocGenerics@60439 bc3139a8-67e5-0310-9ffc-ced21a209358

---
+++ R/unique.R
@@ -0,0 +1,12 @@
+### =========================================================================
+### The unique() generic
+### -------------------------------------------------------------------------
+###
+### Need to explicitly define this generic otherwise the implicit generic in
+### package "base" would dispatch on ('x', 'incomparables'). Here we set
+### dispatch on the 1st arg (the 'x' arg) only!
+
+setGeneric("unique", signature="x",
+    function(x, incomparables=FALSE, ...) standardGeneric("unique")
+)
+
