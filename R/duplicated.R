53227cbac85d1a7b19bfd9aa0b767061a9365801
Herve Pages <hpages@fhcrc.org>
2011-11-17 06:53:53 +0000

    Add the duplicated() and anyDuplicated() generics.
    git-svn-id: file:///home/git/hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/BiocGenerics@60436 bc3139a8-67e5-0310-9ffc-ced21a209358

---
+++ R/duplicated.R
@@ -0,0 +1,16 @@
+### =========================================================================
+### The duplicated() and anyDuplicated() generics
+### -------------------------------------------------------------------------
+###
+### Need to explicitly define those generics otherwise the implicit generics
+### in package "base" would dispatch on ('x', 'incomparables'). Here we set
+### dispatch on the 1st arg (the 'x' arg) only!
+
+setGeneric("duplicated", signature="x",
+    function(x, incomparables=FALSE, ...) standardGeneric("duplicated")
+)
+
+setGeneric("anyDuplicated", signature="x",
+    function(x, incomparables=FALSE, ...) standardGeneric("anyDuplicated")
+)
+
