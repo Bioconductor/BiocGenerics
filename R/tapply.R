b8311c68b9de822d019b3c8a3a35a29fe6e15c6d
Herve Pages <hpages@fhcrc.org>
2011-11-17 05:33:46 +0000

    Add the tapply() generic.
    git-svn-id: file:///home/git/hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/BiocGenerics@60433 bc3139a8-67e5-0310-9ffc-ced21a209358

---
+++ R/tapply.R
@@ -0,0 +1,12 @@
+### =========================================================================
+### The tapply() generic
+### -------------------------------------------------------------------------
+###
+### Need to explicitly define this generic otherwise the implicit generic in
+### package "base" would dispatch on all its arguments. Here we set dispatch
+### on the 1st arg (the 'X' arg) only!
+
+setGeneric("tapply", signature="X",
+    function(X, INDEX, FUN=NULL, ..., simplify=TRUE) standardGeneric("tapply")
+)
+
