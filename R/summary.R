d7bc16c9b8cfd24cf29e52a2d4cf8c3504b64c9e
Herve Pages <hpages@fhcrc.org>
2012-02-21 23:00:44 +0000

    Add summary() and plots() generics.
    git-svn-id: file:///home/git/hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/BiocGenerics@62961 bc3139a8-67e5-0310-9ffc-ced21a209358

---
+++ R/summary.R
@@ -0,0 +1,9 @@
+### =========================================================================
+### The summary() generic
+### -------------------------------------------------------------------------
+
+### summary() is an S3 generic defined in the base package. Here we make it
+### an S4 generic.
+
+setGeneric("plot")
+
