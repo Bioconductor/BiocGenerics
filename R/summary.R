05f8d800c223e26f249c71b3ea413ca7a288962d
Herve Pages <hpages@fhcrc.org>
2012-03-01 20:38:39 +0000

    Remove plot() and summary() generics (they're already defined in the stats4 package).
    git-svn-id: file:///home/git/hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/BiocGenerics@63327 bc3139a8-67e5-0310-9ffc-ced21a209358

--- R/summary.R
+++
@@ -1,9 +0,0 @@
-### =========================================================================
-### The summary() generic
-### -------------------------------------------------------------------------
-
-### summary() is an S3 generic defined in the base package. Here we make it
-### an S4 generic.
-
-setGeneric("summary")
-
