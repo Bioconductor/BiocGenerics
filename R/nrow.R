2fa98a86c70dc0a1dc44537f3c3505639bfe9805
Herve Pages <hpages@fhcrc.org>
2012-03-01 21:15:16 +0000

    Add the nrow(), ncol(), NROW() and NCOL() generics.
    git-svn-id: file:///home/git/hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/BiocGenerics@63328 bc3139a8-67e5-0310-9ffc-ced21a209358

---
+++ R/nrow.R
@@ -0,0 +1,12 @@
+### =========================================================================
+### The nrow(), ncol(), NROW() and NCOL() generics
+### -------------------------------------------------------------------------
+
+### The corresponding functions are standard functions defined in the base
+### package.
+
+setGeneric("nrow")
+setGeneric("ncol")
+setGeneric("NROW")
+setGeneric("NCOL")
+
