e9aad7fe42d9a2ddab482afaab03ea5425b0b64f
Herve Pages <hpages@fhcrc.org>
2012-03-02 10:00:44 +0000

    Add the strand() generic.
    git-svn-id: file:///home/git/hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/BiocGenerics@63352 bc3139a8-67e5-0310-9ffc-ced21a209358

---
+++ R/strand.R
@@ -0,0 +1,8 @@
+### =========================================================================
+### The strand() and `strand<-`() generics
+### -------------------------------------------------------------------------
+
+setGeneric("strand", function(x) standardGeneric("strand"))
+
+setGeneric("strand<-", function(x, value) standardGeneric("strand<-"))
+
