130913a1e740c0e36dcfd7f12816246c68164abe
Herve Pages <hpages@fhcrc.org>
2012-03-02 06:23:49 +0000

    Add the residuals() generic.
    git-svn-id: file:///home/git/hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/BiocGenerics@63347 bc3139a8-67e5-0310-9ffc-ced21a209358

---
+++ R/residuals.R
@@ -0,0 +1,9 @@
+### =========================================================================
+### The residuals() generic
+### -------------------------------------------------------------------------
+
+### The corresponding function is an S3 generic function defined in the
+### stats package.
+
+setGeneric("residuals")
+
