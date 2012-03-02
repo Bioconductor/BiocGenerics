b6445da59a829aed2c087489f2d7a97c56c16b35
Herve Pages <hpages@fhcrc.org>
2012-03-02 01:39:38 +0000

    Add the boxplot() and image() generics.
    git-svn-id: file:///home/git/hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/BiocGenerics@63344 bc3139a8-67e5-0310-9ffc-ced21a209358

---
+++ R/boxplot.R
@@ -0,0 +1,9 @@
+### =========================================================================
+### The boxplot() generic
+### -------------------------------------------------------------------------
+
+### The corresponding function is an S3 generic function defined in the
+### graphics package.
+
+setGeneric("boxplot")
+
---
+++ R/image.R
@@ -0,0 +1,9 @@
+### =========================================================================
+### The image() generic
+### -------------------------------------------------------------------------
+
+### The corresponding function is an S3 generic function defined in the
+### graphics package.
+
+setGeneric("image")
+
