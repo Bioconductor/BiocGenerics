1df827ffd5f041e73542fca60ac522730f563426
Herve Pages <hpages@fhcrc.org>
2012-12-21 23:40:10 +0000

    Register S3 classes AsIs, table, and xtabs, as S4 classes.
    git-svn-id: file:///home/git/hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/BiocGenerics@72138 bc3139a8-67e5-0310-9ffc-ced21a209358

--- R/S3-classes-as-S4-classes.R
+++ R/S3-classes-as-S4-classes.R
@@ -1,8 +1,13 @@
 ### =========================================================================
-### Connections as S4 classes
+### S3 classes as S4 classes
 ### -------------------------------------------------------------------------
 ###
+### We register some old-style (aka S3) classes as formally defined (aka S4)
+### classes. This allows S4 methods defined in Bioconductor packages to use
+### them in their signatures. Note that dispatch still works without this
+### registration but causes 'R CMD INSTALL' to (gently) complain.
 
+### connection class and subclasses
 .connectionClasses <- c("file", "url", "gzfile", "bzfile", "unz", "pipe",
                         "fifo", "sockconn", "terminal", "textConnection",
                         "gzcon")
@@ -10,3 +15,8 @@ apply(cbind(.connectionClasses, "connection"), 1, setOldClass,
       where = environment())
 
 setClassUnion("characterORconnection", c("character", "connection"))
+
+### others
+setOldClass("AsIs")
+setOldClass("xtabs", "table")
+
