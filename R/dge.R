4f058b31ade52f18e98abb00a2e4f0891518fda4
Wolfgang Huber <w.huber@dkfz-heidelberg.de>
2013-09-10 16:47:03 +0000

    New generic functions for DE(X)Seq, provided by Alejandro
    git-svn-id: file:///home/git/hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/BiocGenerics@80209 bc3139a8-67e5-0310-9ffc-ced21a209358

---
+++ R/dge.R
@@ -0,0 +1,18 @@
+# Currently, these are for DESeq and DEXSeq. Could be extended to a more general
+#  infrastructure for count datasets.
+setGeneric("counts",        function(object, ...)        standardGeneric("counts"))
+setGeneric("counts<-",      function(object, ..., value) standardGeneric("counts<-"))
+setGeneric("dispTable",     function(object, ...)        standardGeneric("dispTable"))
+setGeneric("dispTable<-",   function(object, ..., value) standardGeneric("dispTable<-"))
+setGeneric("sizeFactors",   function(object, ...)        standardGeneric("sizeFactors"))
+setGeneric("sizeFactors<-", function(object, ..., value) standardGeneric("sizeFactors<-"))
+
+setGeneric("conditions",    function(object, ...)        standardGeneric("conditions"))
+setGeneric("conditions<-",  function(object, ..., value) standardGeneric("conditions<-"))
+setGeneric("design",        function(object, ...)        standardGeneric("design"))
+setGeneric("design<-",      function(object, ..., value) standardGeneric("design<-"))
+
+setGeneric("estimateSizeFactors", function(object, ...) standardGeneric("estimateSizeFactors"))
+setGeneric("estimateDispersions", function(object, ...) standardGeneric("estimateDispersions"))
+setGeneric("plotDispEsts", function(object, ...) standardGeneric("plotDispEsts"))
+setGeneric("plotMA", function(object, ...) standardGeneric("plotMA"))
