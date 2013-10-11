ac3a46e08f6ca70b0bacb2789aa50c17e514e19e
Michael Lawrence <lawremi@iastate.edu>
2013-10-11 20:11:41 +0000

    add is.unsorted generic
    git-svn-id: file:///home/git/hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/BiocGenerics@81529 bc3139a8-67e5-0310-9ffc-ced21a209358

---
+++ R/is.unsorted.R
@@ -0,0 +1,8 @@
+### =========================================================================
+### The is.unsorted() generic
+### -------------------------------------------------------------------------
+
+setGeneric("is.unsorted",
+           function(x, na.rm = FALSE, strictly = FALSE)
+           standardGeneric("is.unsorted"),
+           signature = "x")
