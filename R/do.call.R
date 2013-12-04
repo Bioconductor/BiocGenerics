ee3f33533c387449de881a233fb0304153f88eba
Herve Pages <hpages@fhcrc.org>
2013-12-04 08:34:05 +0000

    - Add man page for do.call() generic. - Address various cosmetic issues.
    git-svn-id: file:///home/git/hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/BiocGenerics@83873 bc3139a8-67e5-0310-9ffc-ced21a209358

--- R/do.call.R
+++ R/do.call.R
@@ -2,8 +2,9 @@
 ### The do.call() generic
 ### -------------------------------------------------------------------------
 
-setGeneric("do.call",
-           function (what, args, quote = FALSE, envir = parent.frame())
-           standardGeneric("do.call"),
-           signature = c("what", "args"))
+### Need to explicitly define this generic otherwise the implicit generic in
+### package "base" would dispatch on all its arguments. Here we set dispatch
+### on the 1st and 2nd args only!
+
+setGeneric("do.call", signature=c("what", "args"))
 
