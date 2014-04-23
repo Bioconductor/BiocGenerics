1e6569b8bcb3a5af64764c38dfc3da2375eaffde
Herve Pages <hpages@fhcrc.org>
2014-04-23 21:23:54 +0000

    - Add rownames<- and colnames<- generics. - Add S4Vectors to Suggests field.
    git-svn-id: file:///home/git/hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/BiocGenerics@89408 bc3139a8-67e5-0310-9ffc-ced21a209358

--- R/row_colnames.R
+++ R/row_colnames.R
@@ -2,11 +2,14 @@
 ### The rownames() and colnames() generics
 ### -------------------------------------------------------------------------
 ###
-### Need to explicitly define those generics otherwise the implicit generics
-### in package "base" would dispatch on 'do.NULL' and 'prefix'. Here we set
-### dispatch on the 1st arg (the 'x' arg) only!
 
+### Dispatch on the 1st arg (the 'x' arg) only!
 setGeneric("rownames", signature="x")
 
+setGeneric("rownames<-")
+
+### Dispatch on the 1st arg (the 'x' arg) only!
 setGeneric("colnames", signature="x")
+
+setGeneric("colnames<-")
 
