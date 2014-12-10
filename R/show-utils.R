30fdf6321aa1e7c7251b453fe700e837a0dc61e4
Michael Lawrence <lawremi@iastate.edu>
2014-12-10 20:59:34 +0000

    ellipsize and friends gain ability to put ellipsis at the start or end (in addition to the middle)
    git-svn-id: file:///home/git/hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/BiocGenerics@97537 bc3139a8-67e5-0310-9ffc-ced21a209358

--- R/show-utils.R
+++ R/show-utils.R
@@ -4,29 +4,40 @@
 ###
 
 labeledLine <-
-  function(label, els, count = TRUE, labelSep = ":", sep = " ", ellipsis = "...")
+  function(label, els, count = TRUE, labelSep = ":", sep = " ", ellipsis = "...",
+           ellipsisPos = c("middle", "end", "start"))
 {
   if (count && !is.null(els))
     label <- paste(label, "(", length(els), ")", sep = "")
   label <- paste(label, labelSep, sep, sep = "")
   width <- getOption("width") - nchar(label)
-  line <- ellipsize(els, width, sep, ellipsis)
+  line <- ellipsis(els, width, sep, ellipsis, ellipsisPos)
   paste(label, line, "\n", sep = "")
 }
 
 ellipsize <-
-  function(obj, width = getOption("width"), sep = " ", ellipsis = "...")
+  function(obj, width = getOption("width"), sep = " ", ellipsis = "...",
+           pos = c("middle", "end", "start"))
 {
+  pos <- match.arg(pos)
   if (is.null(obj))
     obj <- "NULL"
   if (is.factor(obj))
     obj <- as.character(obj)
-  if (length(obj) > 2 * width)
-    obj <- c(head(obj, width), tail(obj, width))
-  str <- encodeString(obj)
   ## get order selectSome() would print
-  half <- seq_len(ceiling(length(obj) / 2))
-  ind <- as.vector(rbind(half, length(obj) - half + 1))
+  if (pos == "middle") {
+    if (length(obj) > 2 * width)
+      obj <- c(head(obj, width), tail(obj, width))
+    half <- seq_len(ceiling(length(obj) / 2L))
+    ind <- as.vector(rbind(half, length(obj) - half + 1L))
+  } else if (pos == "end") {
+    obj <- head(obj, width)
+    ind <- seq_len(length(obj))
+  } else {
+    obj <- tail(obj, width)
+    ind <- rev(seq_len(length(obj)))
+  }
+  str <- encodeString(obj)
   nc <- cumsum(nchar(str[ind]) + nchar(sep)) - nchar(sep)
   last <- findInterval(width, nc)
   if (length(obj) > last) {
@@ -34,30 +45,52 @@ ellipsize <-
     while (last &&
            (nc[last] + nchar(sep)*2^(last>1) + nchar(ellipsis)) > width)
       last <- last - 1L
-    if (last == 0) ## have to truncate the first element
-      str <-
-        paste(substring(str[1L], 1, width - nchar(ellipsis)), ellipsis,
-              sep = "")
-    else if (last == 1) ## can only show the first
-      str <- c(str[1L], "...")
-    else
-      str <- selectSome(str, last + 1L)
+    if (last == 0) { ## have to truncate the first/last element
+      if (pos == "start") {
+        str <-
+          paste(ellipsis,
+                substring(tail(str, 1L),
+                          nchar(tail(str, 1L))-(width-nchar(ellipsis))+1L,
+                          nchar(ellipsis)),
+                sep = "")
+      } else {
+        str <-
+          paste(substring(str[1L], 1, width - nchar(ellipsis)), ellipsis,
+                sep = "")
+      }
+    }
+    else if (last == 1) { ## can only show the first/last
+      if (pos == "start")
+        str <- c(ellipsis, tail(str, 1L))
+      else str <- c(str[1L], ellipsis)
+    }
+    else {
+      str <- selectSome(str, last + 1L, ellipsis, pos)
+    }
   }
   paste(str, collapse = sep)
 }
 
-## taken directly from Biobase
-selectSome <- function(obj, maxToShow = 5) 
+## taken directly from Biobase, then added 'ellipsisPos' argument
+selectSome <- function(obj, maxToShow = 5, ellipsis = "...",
+                       ellipsisPos = c("middle", "end", "start")) 
 {
+  ellipsisPos <- match.arg(ellipsisPos)
   len <- length(obj)
   if (maxToShow < 3) 
     maxToShow <- 3
   if (len > maxToShow) {
     maxToShow <- maxToShow - 1
-    bot <- ceiling(maxToShow/2)
-    top <- len - (maxToShow - bot - 1)
-    nms <- obj[c(1:bot, top:len)]
-    c(as.character(nms[1:bot]), "...", as.character(nms[-c(1:bot)]))
+    if (ellipsisPos == "end") {
+      c(head(obj, maxToShow), ellipsis)
+    } else if (ellipsisPos == "start") {
+      c(ellipsis, tail(obj, maxToShow))
+    } else {
+      bot <- ceiling(maxToShow/2)
+      top <- len - (maxToShow - bot - 1)
+      nms <- obj[c(1:bot, top:len)]
+      c(as.character(nms[1:bot]), ellipsis, as.character(nms[-c(1:bot)]))
+    }
   } else {
     obj
   }
