1c8749bedd3e516bec7cf8f47d295eff7166216c
Michael Lawrence <lawremi@iastate.edu>
2015-05-21 22:58:01 +0000

    add grep, grepl generics
    git-svn-id: file:///home/git/hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/BiocGenerics@104067 bc3139a8-67e5-0310-9ffc-ced21a209358

---
+++ R/grep.R
@@ -0,0 +1,19 @@
+### =========================================================================
+### The grep() and grepl() generics
+### -------------------------------------------------------------------------
+###
+### Need to explicitly define those generics otherwise the implicit generics
+### in package "base" would dispatch on all their arguments. Here we set
+### dispatch on the first 2 args ('pattern', 'x').
+
+setGeneric("grep",
+           function (pattern, x, ignore.case = FALSE, perl = FALSE,
+                     value = FALSE, fixed = FALSE, useBytes = FALSE,
+                     invert = FALSe) standardGeneric("grep"),
+           signature = c("pattern", "x"))
+
+setGeneric("grepl",
+           function (pattern, x, ignore.case = FALSE, perl = FALSE,
+                     fixed = FALSE, useBytes = FALSE) standardGeneric("grepl"),
+           signature = c("pattern", "x"))
+
