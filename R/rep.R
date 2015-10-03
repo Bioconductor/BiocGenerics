f14eb4a31b7157a44dbae7e1bf1695d9fcfa6fba
Michael Lawrence <lawremi@iastate.edu>
2015-10-03 19:17:25 +0000

    drop special rep.int generic, as R CMD check no longer confuses S4 generics for S3 methods.
    git-svn-id: file:///home/git/hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/BiocGenerics@109158 bc3139a8-67e5-0310-9ffc-ced21a209358

--- R/rep.R
+++ R/rep.R
@@ -1,25 +1,6 @@
 ### =========================================================================
-### The rep() and rep.int() generics
+### The rep.int() generic
 ### -------------------------------------------------------------------------
 
-### A more natural (and cleaner) thing to do for this generic would be to
-### use the same arguments as base::rep.int() (i.e. 'x', 'times') but then
-### 'R CMD check' would get confused and think that we are trying to define
-### an S3 method for base::rep and would complain (observed with R <= 2.12):
-###
-###   * checking S3 generic/method consistency ... WARNING
-###   rep:
-###     function(x, ...)
-###   rep.int:
-###     function(x, times)
-###
-### so we use the arguments of base::rep() (i.e. 'x', '...') just to make
-### 'R CMD check' happy :-) ... Kind of an ugly/silly hack though :-(
-
-.rep.int.useAsDefault <- function(x, ...) base::rep.int(x, ...)
-
-setGeneric("rep.int",
-    function(x, ...) standardGeneric("rep.int"),
-    useAsDefault=.rep.int.useAsDefault
-)
+setGeneric("rep.int")
 
