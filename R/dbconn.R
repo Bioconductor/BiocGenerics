623b6e9ad420c62d6c47071295043821a359908f
Valerie Obenchain <vobencha@fhcrc.org>
2015-10-05 18:45:09 +0000

    move dbconn() and dbfile() generics from AnnotationDbi to BiocGenerics
    git-svn-id: file:///home/git/hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/BiocGenerics@109206 bc3139a8-67e5-0310-9ffc-ced21a209358

---
+++ R/dbconn.R
@@ -0,0 +1,8 @@
+### =========================================================================
+### The dbconn() and dbfile() generics
+### -------------------------------------------------------------------------
+###
+
+setGeneric("dbconn", function(x) standardGeneric("dbconn"))
+
+setGeneric("dbfile", function(x) standardGeneric("dbfile"))
