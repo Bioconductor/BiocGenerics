d9421a7237e2672a8aef1640a6b04f9536f13b44
Herve Pages <hpages@fhcrc.org>
2015-11-05 18:23:44 +0000

    add ellipsis to is.unsorted() generic
    git-svn-id: file:///home/git/hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/BiocGenerics@110355 bc3139a8-67e5-0310-9ffc-ced21a209358

--- R/is.unsorted.R
+++ R/is.unsorted.R
@@ -2,4 +2,16 @@
 ### The is.unsorted() generic
 ### -------------------------------------------------------------------------
 
-setGeneric("is.unsorted", signature = "x")
+### base::is.unsorted() doesn't have the ellipsis. We add it to the generic
+### function defined below so methods can support additional arguments (e.g.
+### the 'ignore.strand' argument for the method for GenomicRanges objects).
+
+.is.unsorted.useAsDefault <- function(x, na.rm=FALSE, strictly=FALSE, ...)
+    base::is.unsorted(x, na.rm, strictly, ...)
+
+setGeneric("is.unsorted", signature="x",
+    function(x, na.rm=FALSE, strictly=FALSE, ...)
+        standardGeneric("is.unsorted"),
+    useAsDefault=.is.unsorted.useAsDefault
+)
+
