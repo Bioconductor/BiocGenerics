205e3d3a7db01aa373621f66e27de43814752965
Herve Pages <hpages@fhcrc.org>
2016-04-06 16:49:14 +0000

    base::order() got a new argument ('method') in R 3.3
    git-svn-id: file:///home/git/hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/BiocGenerics@115903 bc3139a8-67e5-0310-9ffc-ced21a209358

--- R/order.R
+++ R/order.R
@@ -3,7 +3,7 @@
 ### -------------------------------------------------------------------------
 ###
 ### Need to explicitly define this generic otherwise the implicit generic in
-### package "base" would dispatch on ('na.last', 'decreasing').
+### package "base" would dispatch on ('na.last', 'decreasing', 'method').
 ###
 ### Note that dispatching on '...' is supported starting with R 2.8.0 only.
 
