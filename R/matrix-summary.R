e593ddb219639c094058e348186d3e2ca0e5a09b
Michael Lawrence <lawremi@iastate.edu>
2016-11-29 21:11:21 +0000

    add mean, var, sd, rowSums, colSums, rowMeans, colMeans, and t generics
    git-svn-id: file:///home/git/hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/BiocGenerics@124586 bc3139a8-67e5-0310-9ffc-ced21a209358

---
+++ R/matrix-summary.R
@@ -0,0 +1,8 @@
+### - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+### Row-level and column-level summary
+###
+
+setGeneric("rowSums", signature="x")
+setGeneric("colSums", signature="x")
+setGeneric("rowMeans", signature="x")
+setGeneric("colMeans", signature="x")
