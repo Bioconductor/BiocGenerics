614076175a97c215a53f3df11610c096cd31f605
Parham Solaimani <parham@bedatadriven.com>
2017-09-18 15:25:25 +0200

    fix logic and style

--- R/replaceSlots.R
+++ R/replaceSlots.R
@@ -12,22 +12,22 @@
 ### we have one).
 ###
 
-unsafe_replaceSlots <- function(object, ..., .slotList = list()) 
-{
-  valid_argnames <- slotNames(object)
-  args <- extraArgsAsList(valid_argnames, ...)
-  listUpdate <- function(object, l) {
-    for (nm in names(l)) {
-      ## Too risky! identical() is not reliable enough e.g. with objects
-      ## that contain external pointers. For example, DNAStringSet("A") and
-      ## DNAStringSet("T") are considered to be identical! identical() needs
-      ## to be fixed first.
-      ##if (identical(slot(object, nm), l[[nm]]))
-      ##  next
-      slot(object, nm, check=FALSE) <- l[[nm]]
+unsafe_replaceSlots <- function(object, ..., .slotList = list()) {
+    valid_argnames <- slotNames(object)
+    args <- extraArgsAsList(valid_argnames, ...)
+    listUpdate <- function(object, l) {
+        for (nm in names(l)) {
+            ## Too risky! identical() is not reliable enough e.g. with objects
+            ## that contain external pointers. For example, DNAStringSet("A") and
+            ## DNAStringSet("T") are considered to be identical! identical() needs
+            ## to be fixed first.
+            ##if (identical(slot(object, nm), l[[nm]]))
+            ##  next
+            slot(object, nm, check=FALSE) <- l[[nm]]
+        }
+        object
     }
-    object
-  }
+    listUpdate(listUpdate(object, args), .slotList)
 }
 
 ### replaceSlots() is essentially a more efficient initialize(), especially
