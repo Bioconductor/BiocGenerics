6e2a09790a15bc872f5682a9dafd9eb8ce20a40c
Hervé Pagès <hpages@fredhutch.org>
2019-05-09 15:36:24 -0700

    Remove rowSums, colSums, rowMeans, colMeans, mean, and t generics
    The implicit generic for each of these functions (i.e. the S4 generic that
    gets automatically created the first time setMethod() is used to define a
    method for the function) is exactly how it should be, that is, it has the
    right arguments and signature. So there is really no need for us to expli-
    citely define these generics upfront in BiocGenerics with setGeneric()
    statements that result in exactly the same generics as these implicit
    generics.

--- R/matrix-summary.R
+++
@@ -1,8 +0,0 @@
-### - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
-### Row-level and column-level summary
-###
-
-setGeneric("rowSums", signature="x")
-setGeneric("colSums", signature="x")
-setGeneric("rowMeans", signature="x")
-setGeneric("colMeans", signature="x")
