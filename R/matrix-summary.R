0527f88abc438b4293a58b8b4e7ac2eff625eccd
Hervé Pagès <hpages@fredhutch.org>
2019-05-11 13:12:23 -0700

    Revert "Remove rowSums, colSums, rowMeans, colMeans, mean, and t generics"
    This reverts commit 6e2a09790a15bc872f5682a9dafd9eb8ce20a40c.
    
    Even though the implicit generics for these functions are exactly how they
    should be, Bioconductor packages that call these functions (with the hope
    that they will be able to dispatch to the appropriate methods) still need
    to be able to import the generics from "somewhere". Explicitly defining
    these generics in BiocGenerics addresses that.
    
    Also simplify setGeneric statements for row/colSums and row/colMeans.

---
+++ R/matrix-summary.R
@@ -0,0 +1,8 @@
+### - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+### Row-level and column-level summary
+###
+
+setGeneric("rowSums")
+setGeneric("colSums")
+setGeneric("rowMeans")
+setGeneric("colMeans")
