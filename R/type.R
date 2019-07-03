a14e29ba529041432e31c447f26d9c1d1a3fdb69
Hervé Pagès <hpages@fredhutch.org>
2019-07-02 18:48:18 -0700

    Add the type() and `type<-`() S4 generics

---
+++ R/type.R
@@ -0,0 +1,10 @@
+### =========================================================================
+### The type() getter and setter
+### -------------------------------------------------------------------------
+###
+
+
+setGeneric("type", function(x) standardGeneric("type"))
+
+setGeneric("type<-", function(x, value) standardGeneric("type<-"))
+
