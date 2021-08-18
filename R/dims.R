fbd9dbf74e59768659f80e9d35114115a6311dc9
Hervé Pagès <hpages.on.github@gmail.com>
2021-08-17 23:27:52 -0700

    Add nrows() and ncols() generics
    - Like base::lengths(), these new generics have the following arguments:
        nrows(x, use.names=TRUE)
        ncols(x, use.names=TRUE)
    
    - Also add the 'use.names' argument to the dims() generic.

--- R/dims.R
+++ R/dims.R
@@ -1,7 +1,17 @@
 ### =========================================================================
-### The dims() generic
+### The dims(), nrows() and ncols() generics
 ### -------------------------------------------------------------------------
 ###
 
-setGeneric("dims", function(x) standardGeneric("dims"))
+setGeneric("dims", signature="x",
+    function(x, use.names=TRUE) standardGeneric("dims")
+)
+
+setGeneric("nrows", signature="x",
+    function(x, use.names=TRUE) standardGeneric("nrows")
+)
+
+setGeneric("ncols", signature="x",
+    function(x, use.names=TRUE) standardGeneric("ncols")
+)
 
