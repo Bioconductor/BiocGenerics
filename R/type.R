d9d88ab56afcfa8b5f23f5f8b08d41262c4d6dbd
Hervé Pagès <hpages.on.github@gmail.com>
2022-07-25 13:21:46 -0700

    Add type() getter and setter methods for ordinary vectors and arrays

--- R/type.R
+++ R/type.R
@@ -6,5 +6,17 @@
 
 setGeneric("type", function(x) standardGeneric("type"))
 
-setGeneric("type<-", function(x, value) standardGeneric("type<-"))
+setGeneric("type<-", signature="x",
+    function(x, value) standardGeneric("type<-")
+)
+
+setMethod("type", "vector", function(x) typeof(x))
+setMethod("type", "array", function(x) typeof(x))
+
+setReplaceMethod("type", "vector",
+    function(x, value) `storage.mode<-`(x, value=value)
+)
+setReplaceMethod("type", "array",
+    function(x, value) `storage.mode<-`(x, value=value)
+)
 
