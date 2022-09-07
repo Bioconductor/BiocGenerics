b43cbe02855c4dd355fb64ff83951676548c8cbb
Hervé Pagès <hpages.on.github@gmail.com>
2022-09-06 22:08:25 -0700

    Add format() generic and method for list

---
+++ R/format.R
@@ -0,0 +1,33 @@
+### =========================================================================
+### The format() generic
+### -------------------------------------------------------------------------
+###
+### base::format is an S3 generic.
+
+setGeneric("format")
+
+### The base package doesn't define a specific format() method for list
+### objects and format.default() does a poor job on a list:
+###
+###   > format(list(1:10))
+###   [1] "1, 2, 3, 4, 5, 6, 7, 8, 9, 10"
+###   > library(IRanges)
+###   > format(list(IRanges(1, 8:9)))
+###   Error in h(simpleError(msg, call)) :
+###     error in evaluating the argument 'obj' in selecting a method for
+###     function 'unname': IRanges objects don't support [[, as.list(),
+###     lapply(), or unlist() at the moment
+###
+### OTOH format.AsIs() does a good job with lists:
+###
+###   > format.AsIs(list(1:10))
+###   [1] "1, 2, 3,...."
+###   > format.AsIs(list(IRanges(1, 8:9)))
+###   [1] "1-8, 1-9"
+###
+### So we define a format() method for list objects that does that.
+
+### S3/S4 combo for format.list
+format.list <- format.AsIs
+setMethod("format", "list", format.list)
+
