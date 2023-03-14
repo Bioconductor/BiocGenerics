a4b0df9a44e3b009c7a69bdedb99295c4d712e23
Hervé Pagès <hpages.on.github@gmail.com>
2023-03-14 10:51:44 -0700

    small tweak

--- R/type.R
+++ R/type.R
@@ -16,15 +16,18 @@ setMethod("type", "array", function(x) typeof(x))
 
 setMethod("type", "factor", function(x) "character")
 
-### Works on any data-frame-like object for which:
-### - A zero-row object can be extracted via the single-bracket operator (see
-###   below). This includes DataFrame, data.table, and tibble (tbl_df) objects.
-### - as.vector() works on all columns.
-### Note however that the type() method for DataFrame objects (which is
-### defined in the S4Arrays package) does not call .get_data_frame_type()
-### directly on the object. See the S4Arrays package for the details.
+### Works on a data.frame object for which as.vector() can be applied to
+### all its columns.
+### Additionally, note that .get_data_frame_type() would also work
+### out-of-the-box on any data-frame-like object granted that the object
+### supports x[0L, , drop=FALSE]. This includes DataFrame, data.table,
+### and tibble (tbl_df) objects. However this is not how the type() method
+### for DataFrame objects is implemented. See the S4Arrays package (where
+### this method is implemented) for the details.
 .get_data_frame_type <- function(x)
 {
+    if (ncol(x) == 0L)
+        return("logical")
     x0 <- x[0L, , drop=FALSE]
     ## Apply as.vector() on each column to turn them into ordinary vectors.
     ## In particular this will turn columns that are factors into character
